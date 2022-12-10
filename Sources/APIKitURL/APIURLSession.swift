a24efcc5383013cb3937326d5980d00e5370e8f1
Jaehong Kang <sinoru@me.com>
2022-12-10 14:07:04 +0900

    Merges request methods

--- Sources/APIKitURL/APIURLSession.swift
+++ Sources/APIKitURL/APIURLSession.swift
@@ -30,7 +30,6 @@ extension APIURLSession: Session {
 }
 
 extension APIURLSession {
-    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
     public func request(_ request: URLRequestable) async throws -> (URLRequestable.RequestPayload, URLResponse) {
         let urlRequest = request.urlRequest
 
@@ -54,64 +53,12 @@ extension APIURLSession {
         }
     }
 
-    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
     public func request(_ request: URLRequestable) async throws -> URLRequestable.RequestPayload {
         try await self.request(request).0
-    }
-
-    public func request(_ request: URLRequestable, completionHandler: @escaping (Result<(URLRequestable.RequestPayload, URLResponse), Error>) -> Void) {
-        let urlRequest = request.urlRequest
-
-        do {
-            switch try request.urlRequestType {
-            case .data:
-                urlSession.dataTask(with: urlRequest) { data, urlResponse, error in
-                    guard let data, let urlResponse else {
-                        completionHandler(.failure(error!))
-                        return
-                    }
-
-                    completionHandler(.success((.data(data), urlResponse)))
-                }.resume()
-            case .upload(.data(let data)):
-                urlSession.uploadTask(with: urlRequest, from: data) { data, urlResponse, error in
-                    guard let data, let urlResponse else {
-                        completionHandler(.failure(error!))
-                        return
-                    }
-
-                    completionHandler(.success((.data(data), urlResponse)))
-                }
-                .resume()
-            case .upload(.file(let fileURL)):
-                urlSession.uploadTask(with: urlRequest, fromFile: fileURL) { data, urlResponse, error in
-                    guard let data, let urlResponse else {
-                        completionHandler(.failure(error!))
-                        return
-                    }
-
-                    completionHandler(.success((.data(data), urlResponse)))
-                }
-                .resume()
-            case .download:
-                urlSession.downloadTask(with: urlRequest) { url, urlResponse, error in
-                    guard let url, let urlResponse else {
-                        completionHandler(.failure(error!))
-                        return
-                    }
-
-                    completionHandler(.success((.file(url), urlResponse)))
-                }
-                .resume()
-            }
-        } catch {
-            completionHandler(.failure(error))
-        }
     }
 }
 
 extension APIURLSession {
-    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
     @_disfavoredOverload
     public func request<R: APIURLRequestable>(_ request: R) async throws -> (R.ResponseBodyType, URLResponse) {
         let response: (URLRequestable.RequestPayload, URLResponse) = try await self.request(request)
@@ -119,20 +66,8 @@ extension APIURLSession {
         return try (request.responseBodyDecoder.decode(R.ResponseBodyType.self, from: response.0.data), response.1)
     }
 
-    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
     @_disfavoredOverload
     public func request<R: APIURLRequestable>(_ request: R) async throws -> R.ResponseBodyType {
         try await self.request(request).0
-    }
-
-    @_disfavoredOverload
-    public func request<R: APIURLRequestable>(_ request: R, completionHandler: @escaping (Result<(R.ResponseBodyType, URLResponse), Error>) -> Void) {
-        self.request(request as URLRequestable) { result in
-            completionHandler(
-                result.tryMap {
-                    try (request.responseBodyDecoder.decode(R.ResponseBodyType.self, from: $0.0.data), $0.1)
-                }
-            )
-        }
     }
 }
---
+++ Sources/APIKitURL/Foundation.URLSession+APIKitURL.swift
@@ -0,0 +1,253 @@
+//
+//  Foundation.URLSession+APIKitURL.swift
+//  
+//
+//  Created by Jaehong Kang on 2022/12/10.
+//
+
+import Foundation
+
+extension URLSession {
+    private actor TaskManager {
+        var task: URLSessionTask?
+
+        func cancel() async {
+            while true {
+                guard let task = task else {
+                    await Task.yield()
+                    continue
+                }
+
+                task.cancel()
+                break
+            }
+        }
+    }
+
+    /// Convenience method to load data using an URLRequest, creates and resumes an URLSessionDataTask internally.
+    ///
+    /// - Parameter request: The URLRequest for which to load data.
+    /// - Parameter delegate: Task-specific delegate.
+    /// - Returns: Data and response.
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        func task() async throws -> (Data, URLResponse) {
+            let taskManager = TaskManager()
+
+            return try await withTaskCancellationHandler {
+                try await withCheckedThrowingContinuation { continuation in
+                    let task = dataTask(with: request) { data, urlResponse, error in
+                        guard let data, let urlResponse else {
+                            continuation.resume(throwing: error!)
+                            return
+                        }
+
+                        continuation.resume(returning: (data, urlResponse))
+                    }
+
+                    Task {
+                        await taskManager.run { taskManager in
+                            taskManager.task = task
+                        }
+                    }
+                    task.resume()
+                }
+            } onCancel: {
+                Task {
+                    await taskManager.cancel()
+                }
+            }
+        }
+
+        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+        if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
+            return try await data(for: request, delegate: nil)
+        } else {
+            return try await task()
+        }
+        #else
+        return try task()
+        #endif
+    }
+
+    /// Convenience method to upload data using an URLRequest, creates and resumes an URLSessionUploadTask internally.
+    ///
+    /// - Parameter request: The URLRequest for which to upload data.
+    /// - Parameter fileURL: File to upload.
+    /// - Parameter delegate: Task-specific delegate.
+    /// - Returns: Data and response.
+    func upload(for request: URLRequest, fromFile fileURL: URL) async throws -> (Data, URLResponse) {
+        func task() async throws -> (Data, URLResponse) {
+            let taskManager = TaskManager()
+
+            return try await withTaskCancellationHandler {
+                try await withCheckedThrowingContinuation { continuation in
+                    let task = uploadTask(with: request, fromFile: fileURL) { data, urlResponse, error in
+                        guard let data, let urlResponse else {
+                            continuation.resume(throwing: error!)
+                            return
+                        }
+
+                        continuation.resume(returning: (data, urlResponse))
+                    }
+
+                    Task {
+                        await taskManager.run { taskManager in
+                            taskManager.task = task
+                        }
+                    }
+                    task.resume()
+                }
+            } onCancel: {
+                Task {
+                    await taskManager.cancel()
+                }
+            }
+        }
+
+        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+        if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
+            return try await upload(for: request, fromFile: fileURL, delegate: nil)
+        } else {
+            return try await task()
+        }
+        #else
+        return try task()
+        #endif
+    }
+
+    /// Convenience method to upload data using an URLRequest, creates and resumes an URLSessionUploadTask internally.
+    ///
+    /// - Parameter request: The URLRequest for which to upload data.
+    /// - Parameter bodyData: Data to upload.
+    /// - Parameter delegate: Task-specific delegate.
+    /// - Returns: Data and response.
+    func upload(for request: URLRequest, from bodyData: Data) async throws -> (Data, URLResponse) {
+        func task() async throws -> (Data, URLResponse) {
+            let taskManager = TaskManager()
+
+            return try await withTaskCancellationHandler {
+                try await withCheckedThrowingContinuation { continuation in
+                    let task = uploadTask(with: request, from: bodyData) { data, urlResponse, error in
+                        guard let data, let urlResponse else {
+                            continuation.resume(throwing: error!)
+                            return
+                        }
+
+                        continuation.resume(returning: (data, urlResponse))
+                    }
+
+                    Task {
+                        await taskManager.run { taskManager in
+                            taskManager.task = task
+                        }
+                    }
+                    task.resume()
+                }
+            } onCancel: {
+                Task {
+                    await taskManager.cancel()
+                }
+            }
+        }
+
+        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+        if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
+            return try await upload(for: request, from: bodyData, delegate: nil)
+        } else {
+            return try await task()
+        }
+        #else
+        return try task()
+        #endif
+    }
+
+    /// Convenience method to download using an URLRequest, creates and resumes an URLSessionDownloadTask internally.
+    ///
+    /// - Parameter request: The URLRequest for which to download.
+    /// - Parameter delegate: Task-specific delegate.
+    /// - Returns: Downloaded file URL and response. The file will not be removed automatically.
+    func download(for request: URLRequest) async throws -> (URL, URLResponse) {
+        func task() async throws -> (URL, URLResponse) {
+            let taskManager = TaskManager()
+
+            return try await withTaskCancellationHandler {
+                try await withCheckedThrowingContinuation { continuation in
+                    let task = downloadTask(with: request) { url, urlResponse, error in
+                        guard let url, let urlResponse else {
+                            continuation.resume(throwing: error!)
+                            return
+                        }
+
+                        continuation.resume(returning: (url, urlResponse))
+                    }
+
+                    Task {
+                        await taskManager.run { taskManager in
+                            taskManager.task = task
+                        }
+                    }
+                    task.resume()
+                }
+            } onCancel: {
+                Task {
+                    await taskManager.cancel()
+                }
+            }
+        }
+
+        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+        if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
+            return try await download(for: request, delegate: nil)
+        } else {
+            return try await task()
+        }
+        #else
+        return try task()
+        #endif
+    }
+
+    /// Convenience method to resume download, creates and resumes an URLSessionDownloadTask internally.
+    ///
+    /// - Parameter resumeData: Resume data from an incomplete download.
+    /// - Parameter delegate: Task-specific delegate.
+    /// - Returns: Downloaded file URL and response. The file will not be removed automatically.
+    func download(resumeFrom resumeData: Data) async throws -> (URL, URLResponse) {
+        func task() async throws -> (URL, URLResponse) {
+            let taskManager = TaskManager()
+
+            return try await withTaskCancellationHandler {
+                try await withCheckedThrowingContinuation { continuation in
+                    let task = downloadTask(withResumeData: resumeData) { url, urlResponse, error in
+                        guard let url, let urlResponse else {
+                            continuation.resume(throwing: error!)
+                            return
+                        }
+
+                        continuation.resume(returning: (url, urlResponse))
+                    }
+
+                    Task {
+                        await taskManager.run { taskManager in
+                            taskManager.task = task
+                        }
+                    }
+                    task.resume()
+                }
+            } onCancel: {
+                Task {
+                    await taskManager.cancel()
+                }
+            }
+        }
+
+        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+        if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
+            return try await download(resumeFrom: resumeData, delegate: nil)
+        } else {
+            return try await task()
+        }
+        #else
+        return try task()
+        #endif
+    }
+}
